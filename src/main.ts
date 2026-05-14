875db1a21f52d08fa818697ec924cbcd6fa1ac28
ziga <ziga.trop@gmail.com>
2026-05-14 12:29:52 +0200

    xd

--- src/main.ts
+++ src/main.ts
@@ -57,6 +57,7 @@ app.use(cors({
                 }
         },
         credentials: true,
+        optionsSuccessStatus: 200,
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
         allowedHeaders: ['Content-Type', 'Authorization']
 }));
